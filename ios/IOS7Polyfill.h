493ce36e08cd6efaf12f1410be1a52a322217557
Cristian Lupu <cristian.lupu@ellation.com>
2021-02-12 13:56:10 +0200

    Fix iOS rn-fetch-blob warnings
    Jira: CXIOS-6232
    Reviewer: dmitrii.medvedev, ilia.gutu
    PR: GH-1

--- ios/IOS7Polyfill.h
+++ ios/IOS7Polyfill.h
@@ -9,19 +9,12 @@
 #ifndef IOS7Polyfill_h
 #define IOS7Polyfill_h
 
+#import <Foundation/Foundation.h>
+
 @interface NSString (Contains)
 
 - (BOOL)RNFBContainsString:(NSString*)other;
 
 @end
 
-@implementation NSString (Contains)
-
-- (BOOL)RNFBContainsString:(NSString*)other {
-    NSRange range = [self rangeOfString:other];
-    return range.length != 0;
-}
-
-
-@end
 #endif /* IOS7Polyfill_h */
